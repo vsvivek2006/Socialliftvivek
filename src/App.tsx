51a93425a455bccee8821492e58f9bd3f79d54dd
Vivek Singh <143882154+vsvivek2006@users.noreply.github.com>
2025-12-04 18:28:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import BookCall from './pages/BookCall';
 import OurImpact from './pages/OurImpact';
 import Packages from './pages/Packages';
 import Blog from './pages/Blog';
-import SEOService from './pages/digital-marketing/SEOService';
+
 
 
 
@@ -66,7 +66,7 @@ function App() {
             <Route path="/packages" element={<Packages />} />
             <Route path="/impact" element={<OurImpact />} />
             <Route path="/growth-services" element={<GrowthServices />} />
-            <Route path="/services/seo" element={<SEOService />} />
+  
 
             <Route path="/offer" element={<Offer />} />
 
